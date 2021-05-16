8586ff9545272067ac883eeebdcddafe256bf6be
Jeff Lill <jeff@lilltek.com>
2021-05-16 10:18:27 -0700

    coding...

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # purge-runs
+
+**INTERNAL USE ONLY:** This GitHub action is not intended for general use.  The only reason 
+why this repo is public is because GitHub requires it.
+
 Purges workflow runs that have reached or exceeded a specified age
---
+++ action.ps1
@@ -0,0 +1,35 @@
+#Requires -Version 7.0 -RunAsAdministrator
+#------------------------------------------------------------------------------
+# FILE:         action.ps1
+# CONTRIBUTOR:  Jeff Lill
+# COPYRIGHT:    Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
+#
+# The contents of this repository are for private use by neonFORGE, LLC. and may not be
+# divulged or used for any purpose by other organizations or individuals without a
+# formal written and signed agreement with neonFORGE, LLC.
+
+# Verify that we're running on a properly configured neonFORGE jobrunner 
+# and import the deployment and action scripts from neonCLOUD.
+
+# NOTE: This assumes that the required [$NC_ROOT/Powershell/*.ps1] files
+#       in the current clone of the repo on the runner are up-to-date
+#       enough to be able to obtain secrets and use GitHub Action functions.
+#       If this is not the case, you'll have to manually pull the repo 
+#       first on the runner.
+
+$ncRoot = $env:NC_ROOT
+
+if ([System.String]::IsNullOrEmpty($ncRoot) -or ![System.IO.Directory]::Exists($ncRoot))
+{
+    throw "Runner Config: neonCLOUD repo is not present."
+}
+
+$ncPowershell = [System.IO.Path]::Combine($ncRoot, "Powershell")
+
+Push-Location $ncPowershell
+. ./includes.ps1
+Pop-Location
+
+# Clear the runner state.
+
+Clear-Directory $env:GITHUB_WORKSPACE
